b8b1d68dfe68cc0b5a68730e45e3f14f4a545a9c
venkat_now <37570179+venkatn7@users.noreply.github.com>
2023-12-16 13:15:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:6h4C43BhFfD3e6DE4G1e2GfE5facFH-3@mysql.railway.internal:3306/railway"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:c1Hf-Gfc1hf5fhc--DAhA6HFfbEf6Cbg@mysql.railway.internal:3306/railway"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
